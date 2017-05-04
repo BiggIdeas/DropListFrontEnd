275bc0ad59201a423eba99a8f60185c1ed52aa4e
Arlin Grijalba <arlin.grijalba@gmail.com>
2017-05-03 17:16:01 -0700

    Droplist grid page shows rows and filter according to sections

--- app/droplist/droplist.detail.controller.js
+++ app/droplist/droplist.detail.controller.js
@@ -1,43 +1,58 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('app.core')
-    .controller('DroplistDetailController', DroplistDetailController);
+    angular
+      .module('app.core')
+      .controller('DroplistDetailController', DroplistDetailController);
 
-  DroplistDetailController.$inject = ['droplistsFactory', 'departmentsFactory', 'sectionsFactory', '$stateParams'];
+    DroplistDetailController.$inject = ['droplistsFactory', 'departmentsFactory', 'sectionsFactory', '$stateParams'];
 
-  /* @ngInject */
-  function DroplistDetailController(droplistsFactory, departmentsFactory, sectionsFactory, stateParams) {
-    var vm = this;
-    vm.save = save;
-    vm.title = "View Droplists";
-    vm.departmentSections = [];
+    /* @ngInject */
+    function DroplistDetailController(droplistsFactory, departmentsFactory, sectionsFactory, stateParams) {
+      var vm = this;
+      vm.save = save;
+      vm.title = "View Droplists";
+      vm.departmentSections = [];
 
-    activate();
+      activate();
 
-    function activate() {
-      // var customerId = $stateParams.id;
-      //
-      // if (customerId) {
-      //   vm.title = "New Droplist";
-      //   droplistsFactory
-      //     .getById(droplistId)
-      //     .then(function(droplist) {
-      //       vm.droplist = droplist;
-      //     })
-      //     .catch(function(error) {
-      //       alert(error);
-      //     });
-      // }
-      departmentsFactory
-        .getAll()
-        .then(function(departments) {
-          vm.departments = departments;
-        })
-        .catch(function(error) {
-          console.error(error);
-        });
+      function activate() {
+        vm.droplist = {
+          buildingId: 1,
+          driverId: 1,
+          droplistName: "",
+          createdOnDate: new Date(),
+          stockerId: 1, //take logged in stocker id
+          sectionId: 1,
+          stocker_EmployeeId: 1,
+          droplistItems:[]
+          // droplistItems: [{
+          //     aisleNumber: 101,
+          //     aisleRow: 'B',
+          //     aisleColumn: 15,
+          //     itemNumber: 777225,
+          //     description: 'chair',
+          //     quantity: 100
+          //   },
+          //   {
+          //     aisleNumber: 115,
+          //     aisleRow: 'C',
+          //     aisleColumn: 8,
+          //     itemNumber: 55465,
+          //     description: 'table',
+          //     quantity: 100
+          //   }
+          // ]
+        };
+
+        departmentsFactory
+          .getAll()
+          .then(function(departments) {
+            vm.departments = departments;
+          })
+          .catch(function(error) {
+            console.error(error);
+          });
 
         sectionsFactory
           .getAll()
@@ -47,32 +62,41 @@
           .catch(function(error) {
             console.error(error);
           });
-    }
-
-    function save() {
-      // var customerId = $stateParams.id;
-      //
-      // if (droplistId) {
-      //   droplistsFactory
-      //     .update(vm.droplist.droplistId, vm.droplist)
-      //     .then(function() {
-      //       SweetAlert.swal("Droplist saved!", "You did it!", "success");
-      //     })
-      // } else {
-      //   droplistsFactory
-      //     .create(vm.droplist)
-      //     .then(function() {
-      //       SweetAlert.swal("Droplist saved!", "Great Job!", "success");
-      //     });
-      // }
-    }
-
-    vm.filterSections = function filterSections(){
-      vm.departmentSections = [];
-      for (var i = 0; i < vm.sections.length; i++) {
-        if (vm.sections[i].departmentId == vm.selectedDepartment.departmentId)
-          vm.departmentSections.push(vm.sections[i]);
       }
-    }
-  }
-})();
+
+      vm.addItem = function addItem() {
+        vm.droplist.droplistItems.push({});
+      }
+
+      function save() {
+        droplistsFactory
+          .create(vm.droplist)
+          .then(function() {
+              // var droplistId = $stateParams.id;
+              //
+              // if (droplistId) {
+              //   droplistsFactory
+              //     .update(vm.droplist.droplistId, vm.droplist)
+              //     .then(function() {
+              // SweetAlert.swal("Droplist saved!", "You did it!", "success");
+              //     })
+              // } else {
+              //   console.log("print something")
+              //   droplistsFactory
+              //     .create(vm.droplist)
+              //     .then(function() {
+              // SweetAlert.swal("Droplist saved!", "Great Job!", "success");
+              //     });
+              // }
+            });
+          }
+
+        vm.filterSections = function filterSections() {
+          vm.departmentSections = [];
+          for (var i = 0; i < vm.sections.length; i++) {
+            if (vm.sections[i].departmentId == vm.selectedDepartment.departmentId)
+              vm.departmentSections.push(vm.sections[i]);
+          }
+        }
+      }
+    })();
--- app/droplist/droplist.grid.controller.js
+++ app/droplist/droplist.grid.controller.js
@@ -1,20 +1,61 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('app.core')
-        .controller('DroplistGridController', DroplistGridController);
+  angular
+    .module('app.core')
+    .controller('DroplistGridController', DroplistGridController);
 
-        DroplistGridController.$inject = ['droplistsFactory'];
+  DroplistGridController.$inject = ['droplistsFactory', 'departmentsFactory', 'sectionsFactory'];
 
-    /* @ngInject */
-    function DroplistGridController(droplistsFactory) {
-        var vm = this;
+  /* @ngInject */
+  function DroplistGridController(droplistsFactory, departmentsFactory, sectionsFactory) {
+    var vm = this;
 
-        activate();
+    activate();
 
-        function activate() {
+    function activate() {
+      droplistsFactory
+        .getAll()
+        .then(function(droplists) {
+          vm.droplists = droplists;
+        })
+        .catch(function(error) {
+          console.error(error);
+        });
 
-        }
+      departmentsFactory
+        .getAll()
+        .then(function(departments) {
+          vm.departments = departments;
+        })
+        .catch(function(error) {
+          console.error(error);
+        });
+
+      sectionsFactory
+        .getAll()
+        .then(function(sections) {
+          vm.sections = sections;
+        })
+        .catch(function(error) {
+          console.error(error);
+        });
     }
+
+    vm.filterSections = function filterSections() {
+      vm.departmentSections = [];
+      for (var i = 0; i < vm.sections.length; i++) {
+        if (vm.sections[i].departmentId == vm.selectedDepartment.departmentId)
+          vm.departmentSections.push(vm.sections[i]);
+      }
+    }
+
+    vm.filterDroplists = function filterDroplists() {
+      vm.filteredDroplists = [];
+      for (var i = 0; i < vm.droplists.length; i++) {
+        if (vm.droplists[i].sectionId == vm.selectedSection.sectionId)
+          vm.filteredDroplists.push(vm.droplists[i]);
+      }
+    }
+  }
 })();
